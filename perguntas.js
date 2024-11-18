75bbbb445b5ae3b69b9e215e46cb2e0fbecb6a08
16Guilherme <00001113570210sp@al.educacao.sp.gov.br>
2024-11-17 22:20:13 -0300

    Flashcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação que permite criar conteúdo dinâmico, controlar mídias, e animar imagens.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Estados Unidos?',
+    'A capital dos Estados Unidos é Washington, D.C.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é TypeScript?',
+    'TypeScript é uma linguagem de programação de código aberto da Microsoft que usa tipos para descrever objetos e validar código.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em Inglês?',
+    'Love'
+)
